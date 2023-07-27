200d949499abd4e956aed0ae77cb4f7eb80850ba
Jean Lacerda <jean.sena2@gmail.com>
2023-07-26 22:56:01 -0300

    codigo completo

---
+++ src/domain/entity/customer.ts
@@ -0,0 +1,101 @@
+import EventDispatcher from "../event/@shared/event-dispatcher";
+import ChangeAddressEvent from "../event/customer/change-address.event";
+import CustomerCreatedEvent from "../event/customer/customer-created.event";
+import EnviaConsoleLog1Handler from "../event/customer/handler/envia-console-log1.handler";
+import EnviaConsoleLog2Handler from "../event/customer/handler/envia-console-log2.handler";
+import Address from "./address";
+
+
+   
+export default class Customer{
+
+    private _id: string;
+    private _name: string;
+    private _address!: Address;
+	
+	
+
+	private _active: boolean = false;
+	private _rewardPoints: number =0;
+
+    constructor(id: string, name: string, eventDispatcher:EventDispatcher = null) {
+        this._id = id;
+        this._name = name;
+		this.validate();
+		
+		if(eventDispatcher !==null){
+			const customerCreatedEvent: CustomerCreatedEvent = new CustomerCreatedEvent([]);
+			eventDispatcher.notify(customerCreatedEvent);
+		}
+		
+    }
+
+	validate(){
+		if(this._id.length === 0){
+			throw new Error("Id is required")
+		}
+		if(this._name.length === 0 ){
+			throw new Error("Name is required")
+		}
+	}
+
+	changeName(name: string){
+		this._name = name;
+		this.validate();
+	}
+
+	activate() {
+		if(this._address === undefined){
+			throw new Error("Address is mandatory to activate a customer");
+		}
+
+		this._active = true;
+	}
+
+	deactivate(){
+		this._active = false;
+	}
+
+	set Address(address : Address){
+		this._address = address;
+	}
+
+	public get name(): string {
+		return this._name;
+	}
+
+	isActive(): boolean{
+		return this._active;
+	}
+
+	get rewardPoints():number{
+		return this._rewardPoints;
+	}
+
+	addRewardPoints(points:number){
+		this._rewardPoints +=points;
+	}
+
+	changeAddress(address: Address, eventDispatcher:EventDispatcher = null){
+		this._address  = address;
+		if(eventDispatcher !==null){
+			const customerCreatedEvent: ChangeAddressEvent = new ChangeAddressEvent({
+				id: this._id,
+				nome: this._name,
+				endereco: this._address.toString(),
+			});
+			eventDispatcher.notify(customerCreatedEvent);
+		}
+	}
+	 /**
+     * Getter id
+     * @return {string}
+     */
+	 public get id(): string {
+		return this._id;
+	}
+
+	public get Address(): Address {
+		return this._address;
+	}
+}
